777f69e48b7e899a3206610caba67482feafebce
GiaHao810 <giahaobui810@gmail.com>
2022-01-16 12:27:33 +0700

    First Push

---
+++ src/OOP/Customer.java
@@ -0,0 +1,27 @@
+/*
+*  created date: Jan 13, 2022
+*  author: cgm
+*/
+package OOP;
+
+public class Customer {
+
+    //data:
+    String address;
+    String phone;
+    //function
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+    
+}
